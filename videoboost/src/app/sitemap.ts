f2de96ab0da3125d89b6a190aa34b66433bfb187
Neenu <neenulokii2005@gmail.com>
2026-03-26 09:38:46 +0530

    sitemap and SEO added

--- videoboost/src/app/sitemap.ts
+++ videoboost/src/app/sitemap.ts
@@ -9,22 +9,28 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://video-enhance-ai.vercel.app/pricing",
+      url: "https://video-enhance-ai.vercel.app/login",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.8,
     },
     {
-      url: "https://video-enhance-ai.vercel.app/login",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
-    },
-    {
       url: "https://video-enhance-ai.vercel.app/register",
       lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.5,
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://video-enhance-ai.vercel.app/pricing",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: "https://video-enhance-ai.vercel.app/dashboard",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.7,
     },
   ];
 }
